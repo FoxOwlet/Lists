d1217486134d3c93556f1ab995c1cbded1c5ff98
foxowlet <foxowlet.it@gmail.com>
2023-05-30 15:30:47 +0200

    Rename test class

--- src/test/java/com/foxowlet/demo/ListsTest.java
+++ src/test/java/com/foxowlet/demo/ListsTest.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LinkedListTest {
+class ListsTest {
 
     static Stream<Arguments> getImplementations() {
         return Stream.of(
